ac94ea5d02fb2cb201522d11eabaa7da37f03189
Shahid Latif <shahid.latif@uni-due.de>
2018-03-13 13:51:31 +0100

    started file for listing subfields of a community

---
+++ subfields_community.js
@@ -0,0 +1,5 @@
+var subfields_community = {
+	"visualization" : ["information visualization", "scientific visualization", "visual analytics"]
+}; 
+
+var diverse_fields = ["scientific computing", "graphics", "machine learning", "human-computer interaction", "signal processing", "multimedia", "bioinformatics","software engineering", "data management", "geoscience", "graph drawing", "parallel computing"]; 
